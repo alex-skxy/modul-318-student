ae76dfc2c30e65d866bdceb9faced11e9bc44ecf
alex-skxy <alex13skx@gmail.com>
2020-12-02 14:55:05 +0100

    ok so im confused as why the application suddely works without module-info, but hey, at least the tests are working again

---
+++ src/test/java/alexskxy/swisstransport/TransportServiceTest.java
@@ -0,0 +1,35 @@
+package alexskxy.swisstransport;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class TransportServiceTest {
+
+    private ITransport testee;
+
+    @Test
+    public void locations() {
+        testee = new TransportService();
+        var stations = testee.getStations("Sursee,");
+
+        assertEquals(10, stations.stations.size());
+    }
+
+    @Test
+    public void stationBoard() {
+        testee = new TransportService();
+        var stationBoard = testee.getStationBoard("Sursee");
+
+        assertNotNull(stationBoard);
+    }
+
+    @Test
+    public void connections() {
+        testee = new TransportService();
+        var connections = testee.getConnections("Sursee", "Luzern");
+
+        assertNotNull(connections);
+    }
+}
