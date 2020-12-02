099f2465acf9f40fbae2e0f7695c0deca9ab67c9
alex-skxy <alex13skx@gmail.com>
2020-12-02 10:44:44 +0100

    window title

--- src/main/java/alexskxy/transportfinder/Main.java
+++ src/main/java/alexskxy/transportfinder/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application {
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
+        stage.setTitle("TransportFinder");
         stage.show();
     }
 }
